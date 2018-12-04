aa8dcf6888997363546d6795b12fbd471637bfde
Yiannis Nennes <yiannis@nenn.es>
2018-12-04 23:45:30 +0000

    added day 2 part 1 solution

---
+++ 2018/day/2/part1.js
@@ -0,0 +1,31 @@
+// url: https://adventofcode.com/2018/day/2
+const fs = require('fs')
+
+const getLetterFrequency = (data) =>
+  data
+    .split('')
+    .reduce(
+      (freq, letter) => freq.set(letter, freq.has(letter) ? freq.get(letter) + 1 : 1)
+      , new Map())
+
+const mapToValues = (map) =>
+  Array.from(map, ([key, value]) => value)
+
+fs.readFile(`${__dirname}/input.txt`, 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  const letterCount = data
+    .split('\n')
+    .filter(line => line.length)
+    .reduce((count, line) => {
+      const freqValues = mapToValues(getLetterFrequency(line))
+      return {
+        doubleCount: freqValues.includes(2) ? count.doubleCount + 1 : count.doubleCount,
+        tripleCount: freqValues.includes(3) ? count.tripleCount + 1 : count.tripleCount
+      }
+    }, {doubleCount: 0, tripleCount: 0})
+
+  console.log('Solution:', letterCount.doubleCount * letterCount.tripleCount)
+})
